cfb737d1f3dc1256584b631328ff5c6a573a544a
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-05-31 06:24:32 +0100

    0-run

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - check for loop in LL
- * @list: head of linked list
- *
- * Description - check for loops in LL
- * Return: 1 if cycled, 0 if not
+ * check_cycle - checks to see if a list is in an endless loop or cycle
+ * @list: the list to check
+ * Return: 0 if no cycle is detected, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+  listint_t *doub = list;
+  listint_t *reg = list;
 
-	if (!list)
-	{
-		return (0);
-	}
-	slow = list;
-	fast = list->next;
-	while (fast && slow && fast->next)
-	{
-		if (slow == fast)
-		{
-			return (1);
-		}
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-	return (0);
+  if (list == NULL)
+    return (0);
+
+  while (doub && doub->next)
+    {
+      reg = reg->next;
+      doub = doub->next->next;
+
+      if (reg == doub)
+	return (1);
+    }
+  return (0);
 }
