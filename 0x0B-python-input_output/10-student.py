ee2aff621bef34aa29d16434c20305ee2faef7e2
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-07-05 21:36:21 +0100

    10 student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,6 +19,6 @@ class Student:
         for a in attrs:
             try:
                 new_dict[a] = self.__dict__[a]
-            except:
+            except Exception:
                 pass
         return new_dict
